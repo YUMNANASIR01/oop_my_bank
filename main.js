37a0a0315aea0fdb8170a7aebc1b1093aa25e486
YUMNANASIR01 <160057966+YUMNANASIR01@users.noreply.github.com>
2024-07-07 05:59:12 -0700

    Add files via upload
    first_commit

---
+++ main.js
@@ -0,0 +1,50 @@
+class Customer {
+    constructor(firstName, lastName, gender, age, mobileNumber, bankAccount) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.age = age;
+        this.mobileNumber = mobileNumber;
+        this.bankAccount = bankAccount;
+    }
+    CustomerInfo() {
+        return `Name: ${this.firstName} ${this.lastName}
+Age: ${this.age}
+Gender: ${this.gender}
+Mobile: ${this.mobileNumber}
+Account Balance: ${this.bankAccount}`;
+    }
+}
+class BankAccount {
+    constructor(accountBalance) {
+        this.accountBalance = accountBalance;
+    }
+    debit(amount) {
+        let statement = "You entered wrong amount";
+        if (amount > 0) {
+            if (this.accountBalance > amount) {
+                this.accountBalance -= amount;
+                statement = `Transaction Successful, your new balance is ${this.accountBalance}`;
+            }
+            else {
+                statement = "You dont have enough money to do this transaction.";
+            }
+        }
+        return statement;
+    }
+    credit(amount) {
+        let statement = "Transaction Failed";
+        if (amount > 0) {
+            this.accountBalance += amount;
+            statement = "Your acoount has been credited successfully!";
+        }
+        return statement;
+    }
+}
+let myBank = new BankAccount(100);
+console.log(myBank.credit(50));
+console.log(myBank.accountBalance);
+console.log(myBank.debit(70));
+let my_customer = new Customer("Ali", "Anwar", "Male", 22, "3354098", 1800000);
+console.log(my_customer.CustomerInfo());
+export {};
